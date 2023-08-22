7634dd9da5d94f7f65e138d48dc6449e2dea345e
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-08-22 15:23:49 +0200

    initial commit

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+
+$request = $_SERVER;
+
+echo '<pre>';
+print_r($request);
+echo '</pre>';
+
+// $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+
+// define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
+
+// require VIEWS_PATH . 'homepage.php';
