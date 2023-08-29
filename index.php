c8ad82ce7c4a63214c439b681a965b791aa44b7b
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-08-29 12:15:02 +0200

    index.php update

--- index.php
+++ index.php
@@ -3,14 +3,25 @@
 declare(strict_types=1);
 
 require_once('./public/helpers/router.php');
+?>
 
-var_dump($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+<html lang="en">
 
-try {
-    $router->findRoute($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
-} catch (Exception $e) {
-    echo '<pre>';
-    echo $e->getMessage();
-    echo '</pre>';
-    require_once('./public/views/404.php');
-}
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Library</title>
+</head>
+
+<body>
+    <?php
+    try {
+        $router->findRoute($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+    } catch (Exception $e) {
+        require_once('./public/views/404.php');
+    }
+    ?>
+</body>
+
+</html>
