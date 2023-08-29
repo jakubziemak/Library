4c5fff89c90dbf733d24e5228cbf80fdb3a3fc06
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-08-29 12:14:41 +0200

    added simple 404 page

---
+++ public/views/404.php
@@ -0,0 +1,6 @@
+<section class="">
+    <h2 class="text-center text-9xl bg-cyan-800 text-white py-24">404</h2>
+    <p class="text-center text-lg py-16">
+        <?php echo $e->getMessage(); ?>
+    </p>
+</section>
