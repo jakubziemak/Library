0af247e849afb0948c2bf1eb792e744027a11700
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-08-30 15:48:32 +0200

    added new views

--- public/views/home.php
+++ public/views/home.php
@@ -1,10 +1,13 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library</title>
 </head>
+
 <body>
-    
+    <h1>BORKS</h1>
 </body>
+
 </html>
