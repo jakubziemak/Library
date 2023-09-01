38a7aaef4fa2adeaba44eb1df37578ce77b1b21d
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-09-01 13:05:01 +0200

    homeController.php update

--- src/controllers/homeController.php
+++ src/controllers/homeController.php
@@ -1,32 +1,6 @@
 <?php
 
 declare(strict_types=1);
+require_once('./public/views/home.php');
 
-namespace Src\Controllers;
-
-use PDO;
-
-class HomeController
-{
-    private PDO $db;
-
-    public function connectToDb(string $dsn, string $username, string $password)
-    {
-        try {
-            $this->db = new PDO($dsn, $username, $password);
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    public function getFullTable(string $tableName)
-    {
-        $query = "SELECT * FROM $tableName";
-
-        foreach ($this->db->query($query) as $book) {
-            echo "<pre>";
-            print_r($book);
-            echo "/<pre>";
-        }
-    }
-}
+$db->getFullTable('books');
