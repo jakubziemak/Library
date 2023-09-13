4bb288a9c74527216c7c79e8c5a1bbaacbeb1ba9
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-09-13 15:07:58 +0200

    nav.php update

---
+++ public/views/partials/nav.php
@@ -0,0 +1,20 @@
+<header class="bg-cyan-700 px-4 py-8 text-gray-100">
+    <div class="max-w-screen-lg mx-auto flex gap-x-8">
+        <a class="text-3xl grow" href="/">
+            <h1 class="">Library</h1>
+        </a>
+        <form id="search-form" class="m-0 flex">
+            <input type="search" class="px-5 py-1 rounded-l-full bg-gray-100 text-zinc-900" placeholder="Search" />
+            <button type="submit" class="bg-gray-200 px-3 rounded-r-full text-zinc-900 hover:bg-gray-300 hover:text-cyan-800 transition">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                </svg>
+            </button>
+        </form>
+        <nav class="text-lg flex gap-x-4 items-center">
+            <a href="/">Home</a>
+            <a href="create">Create</a>
+            <a href="about">About</a>
+        </nav>
+    </div>
+</header>
