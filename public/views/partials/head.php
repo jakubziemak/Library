479ac97198b74724eaefe47a505e9d4e15189b6d
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-09-13 14:59:35 +0200

    head.php update

--- public/views/partials/head.php
+++ public/views/partials/head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Library</title>
+    <title><?php echo $controller->getPageTitle() ?></title>
 </head>
 
-<body>
+<body class="min-h-screen flex flex-col justify-between">
