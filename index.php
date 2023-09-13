d68edb6935bb241f0279bacb2e3af72ca257e565
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-09-13 12:46:20 +0200

    index.php update

--- index.php
+++ index.php
@@ -2,22 +2,20 @@
 
 declare(strict_types=1);
 
-require_once('./src/db/DataBase.php');
+require_once('./src/database/DataBase.php');
 require_once('./src/router/Route.php');
 require_once('./src/router/Router.php');
 
 use Src\Router\Route;
 use Src\Router\Router;
-use Src\Db\DataBase;
+use Src\Database\DataBase;
 
 $db = new DataBase();
 $db->connectToDb('mysql:host=127.0.0.1:3306;dbname=library', 'root', 'root');
 
-$test = 123;
-
 $router = new Router();
 
-$router->addRoute(new Route('GET', '/', './src/controllers/homeController.php'));
+$router->addRoute(new Route('GET', '/', './public/views/home.php'));
 $router->addRoute(new Route('GET', '/create', './public/views/create.php'));
 
 try {
