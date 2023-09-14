f3c6baee3c919534e79f177c860a346716c5fbe2
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-09-14 12:58:00 +0200

    footer.php fix

--- public/views/partials/footer.php
+++ public/views/partials/footer.php
@@ -1,7 +1,7 @@
-</body>
-
 <footer class="relative bottom-0 bg-cyan-700 px-4 py-8 text-gray-100">
     <p class="max-w-screen-lg mx-auto">Made by Jakub Ziemak, 2023</p>
 </footer>
+
+</body>
 
 </html>
