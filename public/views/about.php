61d7a8f8640364d42b02f798b235a63ad3fd0345
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-09-18 14:13:08 +0200

    added about.php

---
+++ public/views/about.php
@@ -0,0 +1,26 @@
+<?php
+require_once('./public/views/partials/head.php');
+require_once('./public/views/partials/nav.php');
+?>
+
+<main class="grow bg-gray-100 px-4 pb-20 overflow-hidden">
+    <section class="relative text-white py-20 mb-20">
+        <span class="bg-cyan-800 absolute h-full w-screen -ml-4 top-0"></span>
+        <div class="max-w-screen-md mx-auto">
+            <h2 class="relative text-5xl mb-12 max-w-xl">Lorem ipsum dolor sit amet </h2>
+            <p class="relative text-lg max-w-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis a nisl quis egestas. Nunc. </p>
+        </div>
+    </section>
+
+    <section class="max-w-screen-md mx-auto">
+        <h3 class="text-lg mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin gravida.</h3>
+        <p class="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis maximus quam et sodales. Cras consequat rutrum neque, id semper felis posuere id. Donec posuere neque lorem, sed placerat justo consectetur nec. Etiam vel aliquam nunc. Praesent elit nibh, pharetra quis neque sed, cursus pulvinar libero. Etiam commodo, magna ut sodales porttitor, turpis tortor posuere dolor, at commodo libero diam et risus. Aenean viverra viverra metus, vitae bibendum ligula tincidunt vitae. Sed sodales consequat massa, sed iaculis felis maximus sed. Phasellus fermentum facilisis pharetra. Cras nec semper purus. Praesent in varius eros. Aenean quis mollis ex. Pellentesque convallis porta placerat. Sed interdum nisi nunc, in tempus tellus aliquet non. Vestibulum ultrices sapien non viverra vehicula.
+        </p>
+        <p>
+            Nam placerat elementum mollis. Mauris quis malesuada felis, nec lobortis felis. Donec condimentum nibh ipsum, ut faucibus tortor facilisis vel. Curabitur sit amet justo eu nisl varius vestibulum ac at justo. Morbi fermentum placerat eleifend. Fusce egestas lorem nibh, tincidunt euismod mauris laoreet ut. Vestibulum condimentum auctor tellus non mollis. Nulla eleifend est porta, scelerisque nulla in, mollis ipsum. Suspendisse at sapien egestas massa suscipit commodo et mollis libero. Nam commodo, eros vitae accumsan faucibus, tellus lacus blandit dui, eu aliquet orci erat eget odio. Mauris eleifend erat nec cursus rhoncus. Phasellus sit amet feugiat felis, ac suscipit felis. Sed pharetra orci et eros dapibus varius. Sed euismod volutpat risus ac tempus. Duis commodo quis nisi nec vehicula.
+        </p>
+    </section>
+</main>
+
+<?php require_once('./public/views/partials/footer.php'); ?>
