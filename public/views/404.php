8befac666299069811321ff28b74b92aa2c0e7f3
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-09-20 13:08:56 +0200

    404.php update

--- public/views/404.php
+++ public/views/404.php
@@ -4,11 +4,11 @@ require_once('./public/views/partials/nav.php');
 ?>
 
 
-<main class="grow bg-cyan-800 text-white py-24 h-full px-4">
+<main class="grow bg-cyan-800 text-white pt-24 h-full px-4">
     <div class="max-w-screen-md m-auto">
-        <h2 class="text-9xl pb-16">404</h2>
-        <p class="text-6xl py-12">Page not Found</p>
-        <p class="text-3xl pt-12">Sorry, the page you are looking for doesn't exist or has been moved.</p>
+        <h2 class="text-9xl pb-12">404</h2>
+        <p class="text-6xl pb-24">Page not Found</p>
+        <p class="text-3xl ">Sorry, the page you are looking for doesn't exist or has been moved.</p>
     </div>
 </main>
 
