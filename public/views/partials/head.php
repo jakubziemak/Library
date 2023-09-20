1ab246183d12a4957315ec6dee9761d54b424d20
JakubZ97 <84505670+JakubZ97@users.noreply.github.com>
2023-09-20 13:09:23 +0200

    head.php added doctype

--- public/views/partials/head.php
+++ public/views/partials/head.php
@@ -1,10 +1,13 @@
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title><?php echo $controller->getPageTitle() ?></title>
+    <title>
+        <?php echo $_SESSION['pageTitle'] ?>
+    </title>
 </head>
 
 <body class="min-h-screen flex flex-col justify-between">
